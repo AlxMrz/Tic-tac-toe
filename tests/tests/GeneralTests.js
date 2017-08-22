218ae32127c743d7fdf33886299b4ad5b3cd0505
Alexandr Moroz <alexandr.moroz97@mail.ru>
2017-08-22 20:39:26 +0300

    Тесты проходят, но перестал работать проект. Нужно покрыть тестами mainProcess

--- tests/tests/GeneralTests.js
+++ tests/tests/GeneralTests.js
@@ -1,15 +1,5 @@
 QUnit.module("General Tests");
-
-var Cell1 = 0,
-    Cell2 = 0,
-    Cell3 = 0,
-    Cell4 = 0,
-    Cell5 = 0,
-    Cell6 = 0,
-    Cell7 = 0,
-    Cell8 = 0,
-    Cell9 = 0,
-    gameover = false;
+var game = new Game();
     
 let cell = class {
     constructor() {
@@ -20,16 +10,16 @@ let cell = class {
 var player = 'Y';
 
 QUnit.test("Check Cell Function Test", function (assert) {
-    assert.notOk(checkCell(cell));
+    
+    assert.notOk(game.checkCell(cell));
     cell.value = ' ';
-    assert.ok(checkCell(cell));
+    assert.ok(game.checkCell(cell));
 });
 QUnit.test("Change Value Test", function (assert) {
-    changeVal(cell);
+    game.changeVal(cell);
     assert.ok(cell.value === 'Y');
 });
 QUnit.test("Change Cell Value Test", function (assert) {
-    changeCell('cell2');
-    console.log(Cell1);
-    assert.ok(Cell2 === 'Y');
+    game.changeCell('cell2');
+    assert.ok(game.Cell2 === 'Y');
 });
